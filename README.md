c1550ea47b0f9007da086a0b9a378a1560ddac60
katsihuin <katsihuin@gmail.com>
2016-12-28 07:58:52 -0500

    primer avance

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# makes10
+# Objetivo80
+
+##Inicio: Calcular porcentaje de Squad
+
+##Declaración de Variables
+
+- puntosSquad
+- puntosMaximo
+- porcentajeSquad
+
+##Ingreso de Datos
+
+- Pedir la cantidad de puntos por squad
+- Pedir la cantidad puntos maximo disponibles
+
+##Proceso
+
+- Operar (100 * punto_squad) / max_puntos y guardar en porcentaje_squad
+- Si porcentaje_squad es mayor que 80
+    - Mostrar mensaje: "Muy bien Squad!
+- Si no
+    - Monstrar mensaje: "Pueden hacerlo mejor"
+
+## Mostrar Resultados
+
+- Imprimir mensajes
+
+## Fin
---
+++ main.js
@@ -0,0 +1,27 @@
+// Codigo para calcular los puntos squad 
+/*function calcular() {
+    var a = document.getElementById("numeroA").value;
+    var b = document.getElementById("numeroB").value;
+    var neg = document.getElementById("neg").value;
+    var salida = document.getElementById("salida");
+
+	if (a == "" || b == ""){
+        document.getElementById("salida").innerHTML = "<div class='alert'>Ingrese valores</div>";
+    } else {
+        if (neg == "1") {
+            if (a < 0 && b < 0) {
+                salida.innerHTML = "1";
+            } 
+            else {
+                salida.innerHTML = "0";
+            }
+        }   
+        if (neg == "2") {
+            if ( a < 0 || b < 0) {
+               salida.innerHTML = "1";
+            }
+            else {
+               salida.innerHTML = "0";
+            } 
+        }
+*/
