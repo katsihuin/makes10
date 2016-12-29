48a7bcfd4c35d4279cb2737d30abdf9366e51130
katsihuin <katsihuin@gmail.com>
2016-12-28 23:43:14 -0500

    ejercicio terminado

--- main.js
+++ main.js
@@ -3,18 +3,20 @@ function calcular()
 {
     var a = document.getElementById("numA").value;
     var b = document.getElementById("numB").value;
+    var suma = parseInt(a) + parseInt(b);
     var salida = document.getElementById("salida");
 
-	if (a == "" || b == "")
+    if (a == "" || b == "")
     {
         document.getElementById("salida").innerHTML = "<div class='alert'>Ingrese valores</div>";
-    } else if (a == 10 || b == 10 || a+b == 10)
+    } else if (a == 10 || b == 10 || suma == 10)
     {
+        // Uno de los números es 10 o ambos suman 10
         salida.innerHTML = "Si";
     } 
     else 
     {
+        // Ninguno de los números es 10 o ambos tampoco suman 10
         salida.innerHTML = "No";
     }
 }
-  
